d56d879c93da225502d22c337dbb5291c962310a
Koki Nanao <minsokosoko0107@gmail.com>
2023-05-31 23:10:41 +0900

    fix:修正
    サイト情報を修正

--- site.config.ts
+++ site.config.ts
@@ -1,7 +1,7 @@
 // サイトの情報を一元管理
 
 const siteConfig = {
-  title: 'Test Title',
+  title: 'Nanao Output',
   twitterUrl: 'https://twitter.com/nao_funky__',
   githubUrl: 'https://github.com/naofunky',
 };
